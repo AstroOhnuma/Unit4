2eb370452e9b0473eafbf5dcd61cebe74714489f
AstroOhnuma <31485811+AstroOhnuma@users.noreply.github.com>
2017-10-16 14:40:44 -0400

    Updated from Brython Server: 10/16/2017 2:40:43 PM

--- printstars.py
+++ printstars.py
@@ -1,1 +1,11 @@
+#Astro Ohnuma
+#10/16/17
+#printstars.py - takes and integer and then prints out that many stars
 
+def printnstars(num1):
+    total = num1
+    for i in range(1,num1+1):
+        total -= 1
+        print((i*'*'),(total*' '))
+
+printnstars(6)
