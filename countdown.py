ab9eda2491f382206ad0b21b074e1176d96a57fd
AstroOhnuma <31485811+AstroOhnuma@users.noreply.github.com>
2017-10-16 14:45:05 -0400

    Updated from Brython Server: 10/16/2017 2:45:04 PM

--- countdown.py
+++ countdown.py
@@ -1,1 +1,12 @@
+#Astro Ohnuma
+#10/16/17
+#countdownn.py - taking an integer and counting down to zero from that number
 
+def countdownn(num1):
+    i = num1
+    while i >=1:
+        print(i)
+        i -= 1
+    print('BOOM!')
+
+countdownn(10)
