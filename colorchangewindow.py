a2ee753d40cc1b5e2e0b7e918e1440b4ca82433c
AstroOhnuma <31485811+AstroOhnuma@users.noreply.github.com>
2017-10-18 13:26:36 -0400

    Updated from Brython Server: 10/18/2017 1:26:34 PM

--- colorchangewindow.py
+++ colorchangewindow.py
@@ -5,4 +5,8 @@
 from ggame import *
 
 def mouseclick(event):
-    
+    red = Color(0xFF0000,1)
+    green = Color(0x00FF00,1)
+    blue = Color(0x0000FF,1)
+    black = Color(0x000000,1)
+    blackoutline = LineStyle(1,black)
