0e1f4fb6a4f016dde563c21b303d479140fb6c47
AstroOhnuma <31485811+AstroOhnuma@users.noreply.github.com>
2017-10-18 13:35:22 -0400

    Updated from Brython Server: 10/18/2017 1:35:21 PM

--- returndemo.py
+++ returndemo.py
@@ -1,1 +1,14 @@
+#Astro Ohnuma
+#10/18/17
+#returndemo.py - how to use return with a function
+
+from random import randint
+
+def randevenint(low,high):
+    return randint(low,high)
+
+r1 = randevenint(1,100)
+r2 = randevenint(1,100)
+r3 = randevenint(1,100)
+print(r1,r2,r3)
 
