ff403da246ccc0037c50b306a559589068ed63de
AstroOhnuma <31485811+AstroOhnuma@users.noreply.github.com>
2017-10-19 14:16:54 -0400

    Updated from Brython Server: 10/19/2017 2:16:54 PM

--- monkeybanana.py
+++ monkeybanana.py
@@ -1,1 +1,23 @@
+#Astro Ohnuma
+#10/19/17
+#monkeybanana.py - best game ever
 
+from ggame import *
+
+
+ROWS = 40
+COLS = 60
+CELL_SIZE = 20
+
+if __name__ == '__main__':
+    
+    green = Color(0x006600,1)
+    brown = Color(0x8B4513,1)
+    
+    junglebox = RectangleAsset(COLS*CELL_SIZE,ROWS*CELL_SIZE,LineStyle(1,green),green)
+    monkeybox = RectangleAsset(CELL_SIZE,CELL_SIZE,LineStyle(1,brown),brown)
+    
+    Sprite(junglebox)
+    Sprite(monkeybox)
+    
+    App().run()
