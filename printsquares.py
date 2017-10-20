5adfe5b9b96fb40480518d9d91ff23aefd511406
AstroOhnuma <31485811+AstroOhnuma@users.noreply.github.com>
2017-10-20 14:45:33 -0400

    Updated from Brython Server: 10/20/2017 2:45:32 PM

--- printsquares.py
+++ printsquares.py
@@ -3,10 +3,11 @@
 #printsquares.py - printing squares with a function
 
 def printsquares(num1,num2):
-    for i in range(num1,num2):
+    for i in range(1,num2):
         print(('+--'*num2)+('+'))
+    for i in range(1,num1):
         print(('|  |  '*num1)+('|'))
+print(('+--'*num2)+('+'))
 
 
-
-printsquares(2,4)
+printsquares(3,4)
