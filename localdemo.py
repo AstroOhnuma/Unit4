a728e82ff54b2e5079624e7a1d6944b3c669f24e
AstroOhnuma <31485811+AstroOhnuma@users.noreply.github.com>
2017-10-23 14:18:51 -0400

    Updated from Brython Server: 10/23/2017 2:18:50 PM

--- localdemo.py
+++ localdemo.py
@@ -3,10 +3,10 @@
 #localdemo.py - understanding local variables
 
 def f():
-    x = 77
-    y = 10
+    x = 77 #x is a local variable
+    y = 10 #y is also a local variable
 
 x = 5
-f()
+f() #x does not change
 print(x)
-print(y)
+print(y) #this will cause an error]
