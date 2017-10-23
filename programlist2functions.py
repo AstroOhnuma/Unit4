e60cdceb4d1c6fb2e3d04b5211356d85446b10a4
AstroOhnuma <31485811+AstroOhnuma@users.noreply.github.com>
2017-10-23 14:52:11 -0400

    Updated from Brython Server: 10/23/2017 2:52:10 PM

--- programlist2functions.py
+++ programlist2functions.py
@@ -1,1 +1,9 @@
+#Astro Ohnuma
+#10/23/17
+#programlist2functions.py - the functions for program list 2
+
+#8 - taking two strings as arguments and returns a string that contains all the letters that appear in at least one of the words
+
+def stringunion():
+    
 
