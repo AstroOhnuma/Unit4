e0dc0ff2bbb903338ef23b1e5dc3081e60081c0a
AstroOhnuma <31485811+AstroOhnuma@users.noreply.github.com>
2017-10-23 14:46:16 -0400

    Updated from Brython Server: 10/23/2017 2:46:15 PM

--- distance.py
+++ distance.py
@@ -1,6 +1,9 @@
 #Astro Ohnuma
 #10/20/17
 #distance.py - takes two points as agruements and returns the distance between the two points
+from math import sqrt
 
-def distance(num1,num2,num3,num4):
+def distance(x1,y1,x2,y2):
+    sqrt((x2-x1)**2+(y2-y1)**2)
     
+print(distance(3,4,-5,2))
