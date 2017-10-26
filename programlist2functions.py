80403279d95eb48f977e31e53dcd980f584ad8d4
AstroOhnuma <31485811+AstroOhnuma@users.noreply.github.com>
2017-10-26 14:24:06 -0400

    Updated from Brython Server: 10/26/2017 2:24:05 PM

--- programlist2functions.py
+++ programlist2functions.py
@@ -8,9 +8,11 @@ from random import randint
 def stringunion(str1,str2):
     answer = ''
     for ch in str1:
-        print(answer+ch)
+        if ch >= ch:
+            print('no')
+        answer += ch
     for ch in str2:
-        print(answer+ch)
+        answer += ch
     print(answer)
 stringunion('word','other')
 
