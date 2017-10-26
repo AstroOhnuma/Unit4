08f695de04142cce5750479b5e959ae1dbc80ef5
AstroOhnuma <31485811+AstroOhnuma@users.noreply.github.com>
2017-10-26 14:54:01 -0400

    Updated from Brython Server: 10/26/2017 2:54:01 PM

--- programlist2functions.py
+++ programlist2functions.py
@@ -3,6 +3,7 @@
 #programlist2functions.py - the functions for program list 2
 
 #8 - taking two strings as arguments and returns a string that contains all the letters that appear in at least one of the words
+"""
 def stringunion(str1,str2):
     answer = ''
     for ch in str1:
@@ -13,14 +14,15 @@ def stringunion(str1,str2):
             answer += ch
     print(answer)
 stringunion('Mississipi','Pensylvania')
+"""
 #9 - taking two strings as arguments and returns a string that contains all the letters that are in both the first and second word
 def stringintersect(str1,str2):
     answer = ''
     for ch in str1:
-        if not ch in answer:
+        if ch in answer:
             answer += ch
     for ch in str2:
-        if not ch in answer:
+        if ch in answer:
             answer += ch
     print(answer)
 stringintersect('Mississipi','Pensylvania')
