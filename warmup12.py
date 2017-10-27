15c5d067d9e7517094d3102b0f8f6bd3dbcf9b06
AstroOhnuma <31485811+AstroOhnuma@users.noreply.github.com>
2017-10-27 14:39:33 -0400

    Updated from Brython Server: 10/27/2017 2:39:33 PM

--- warmup12.py
+++ warmup12.py
@@ -1,1 +1,9 @@
+#Astro Ohnuma
+#10/27/17
+#warmup12.py - finding greatest common factor
 
+def GCF(num1,num2):
+    for i in range(num1,0,-1):
+        if num1%i==0 and num2%i==0:
+            return i
+print(GCF(10,15))
