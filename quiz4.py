9a72f022ac1dcb430ae344625cfbea661a55cc86
AstroOhnuma <31485811+AstroOhnuma@users.noreply.github.com>
2017-10-30 14:30:28 -0400

    Updated from Brython Server: 10/30/2017 2:30:27 PM

--- quiz4.py
+++ quiz4.py
@@ -1,1 +1,14 @@
+#Astro Ohnuma
+#10/30/17
+#quiz4.py - our fourth quiz
 
+def csia():
+    for i in range(1,6):
+        print('Computer Science is Awesome')
+def average(num1,num2,num3):
+    return (num1+num2+num3)/3
+def lastletter(word):
+    for ch in word:
+        
+csia()
+print(average(1,2,3))
