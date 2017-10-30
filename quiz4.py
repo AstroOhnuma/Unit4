b6b00826b1867625d2ba07ac7f9f0a43af34c6bb
AstroOhnuma <31485811+AstroOhnuma@users.noreply.github.com>
2017-10-30 14:45:07 -0400

    Updated from Brython Server: 10/30/2017 2:45:06 PM

--- quiz4.py
+++ quiz4.py
@@ -7,8 +7,17 @@ def csia():
         print('Computer Science is Awesome')
 def average(num1,num2,num3):
     return (num1+num2+num3)/3
+''' #don't know how to do
 def lastletter(word):
-    for ch in word:
-        
+    if ch in word:
+        print('f')
+def same(num1,num2,num3,num4): #can't figure out how to let people choose the arguements
+    if (num1*num2) == (num3-num4):
+        return True
+    else:
+        return False
+'''
 csia()
-print(average(1,2,3))
+print(average(1,2,3))'''
+lastletter('Smedinghoff')
+print(same(2,3,7,1))'''
